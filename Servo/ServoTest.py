45617d22a12523a994ac75bd50c65e3c3cc5d34e
MartinSimengard <simengardmartin@gmail.com>
2023-10-05 15:34:03 +0200

    Added printing

--- Servo/ServoTest.py
+++ Servo/ServoTest.py
@@ -13,12 +13,17 @@ try:
     
     servos.move_servo_position(TILT_SERVO, 40)
     servos.move_servo_position(PAN_SERVO, 20)
+    print("TILT position: 40")
+    print("PAN position: 20")
+
 
     # Wait for 1 second
     time.sleep(1)
 
     servos.move_servo_position(TILT_SERVO, 60)
     servos.move_servo_position(PAN_SERVO, 90)
+    print("TILT position: 60")
+    print("PAN position: 90")
 
     # Wait for 1 second
     time.sleep(1)
@@ -26,7 +31,3 @@ try:
 except KeyboardInterrupt:
     print("Exiting...")
 
-finally:
-    # Ensure the PWM outputs are off before exiting
-    servos.disable_servo(TILT_SERVO)
-    servos.disable_servo(PAN_SERVO)
